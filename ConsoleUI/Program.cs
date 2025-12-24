ca3d39cf3c389bc40faf7bdbedbca5fdfeaf5fdb
sadovnichek <kapkaevictor@gmail.com>
2025-12-24 17:53:12 +0500

    Initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using TelegramAPI;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var connection = new Connection();
+            await connection.ConnectAsync("www.google.com", 80);
+
+            var requestMessage = $"GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: Close\r\n\r\n";
+            var requestData = Encoding.UTF8.GetBytes(requestMessage);
+            await connection.SendAsync(requestData);
+
+            var received = await connection.ReceiveAsync();
+            Console.WriteLine(received);
+        }
+    }
+}
---
+++ TelegramAPI/Connection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TelegramAPI
+{
+    public class Connection : IDisposable
+    {
+        private readonly TcpClient client;
+        private NetworkStream stream;
+
+        public Connection(string ip, int port)
+        {
+            client = new TcpClient(new IPEndPoint(IPAddress.Parse(ip), port));
+            client.ReceiveTimeout = 1000;
+            client.SendTimeout = 1000;
+        }
+
+        public Connection()
+        {
+            client = new TcpClient();
+        }
+
+        public async Task ConnectAsync(string host, int port)
+        {
+            try
+            {
+                await client.ConnectAsync(host, port);
+                stream = client.GetStream();
+                Console.WriteLine($"Connected to {host}:{port}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to connect to {host}:{port}");
+            }
+        }
+
+        public async Task SendAsync(byte[] data)
+        {
+            await stream.WriteAsync(data);
+        }
+
+        public async Task<string> ReceiveAsync()
+        {
+            var responseData = new byte[1024];
+            var response = new StringBuilder();
+            int bytes;
+
+            do
+            {
+                bytes = await stream.ReadAsync(responseData);
+                response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
+            }
+            while (client.Available > 0);
+
+            return response.ToString();
+        }
+
+        public void Close()
+        {
+            stream.Close();
+            client.Close();
+        }
+
+        public async void Dispose()
+        {
+            await stream.DisposeAsync();
+            client.Dispose();
+        }
+    }
+}
